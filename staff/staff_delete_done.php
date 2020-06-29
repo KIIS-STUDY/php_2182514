db020229120d42427e7042ee64314d7375b15588
Ryoukenhou <liujianfeng6y@gmail.com>
2020-06-29 17:38:53 +0900

    2020/06/29 work

--- staff/staff_delete_done.php
+++ staff/staff_delete_done.php
@@ -11,11 +11,7 @@
 try
 {
 
-    $staff_name = $_POST['name'];
-    $staff_pass = $_POST['pass'];
-
-    $staff_name=htmlspecialchars($staff_name, ENT_QUOTES,'UTF-8');
-    $staff_pass=htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+    $staff_code = $_POST['code'];
 
     $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
     $user='root';
@@ -23,16 +19,12 @@ try
     $dbh=new PDO($dsn,$user,$password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-    $sql='INSERT INTO mst_staff(name,password)VALUES(?,?)';
+    $sql='DELETE FROM mst_staff WHERE code=?';
     $stmt=$dbh->prepare($sql);
-    $data[]=$staff_name;
-    $data[]=$staff_pass;
+    $data[]=$staff_code;
     $stmt->execute($data);
 
     $dbh=null;
-
-    print $staff_name;
-    print 'さんを追加しました。<br/>';
 
 }
  catch(Exception $e)
@@ -43,6 +35,8 @@ try
 
 ?>
 
+削除しました。 <br />
+<br />
 <a href="staff_list.php">戻る</a>
 
 </body>
--- staff/staff_list.php
+++ staff/staff_list.php
@@ -1,47 +1,52 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>ろくまる農園</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <title>ろくまる農園</title>
+        </head>
+        <body>
 
-<?php
+        <?php
 
 try
 {
 
-$dsn ='mysql:dbname= shop;host=localhost;charset=utf8';
-$user = 'root';
-$password = ";
-$dbh = new PDO($dsn,$user,$password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn,$user,$password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$sql = 'SELECT name FROM mst_staff WHERE 1';
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
+    $sql='SELECT code, name FROM mst_staff WHERE 1';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
 
-$dbh = null;
+    $dbh = unll;
 
-print'スタッフ一覧 <br /><br />';
+    print'スタッフ一覧<br /><br/ >';
 
-while(true)
+    print '<form method="post" action="staff_branch.php">';
+    while (true)
 {
-	$rec = $stmt->fetch(PDO::FETCH_ASSOC);
-	if($rec==false)
-	{
-		break;
-	}
-	print$rec['name'];
-	print'<br />';
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($rec==false)
+{
+break;
 }
-
+    print'<input type="radio" name="staffcode" value="'.$rec['code'].'">';
+    print $rec['name'];
+    print'<br />';
+}
+    print'<input type="submit"  name="edit" value="修正">';
+    print'<input type="submit" name="delete"value="削除">';
+    print'</form>';
 }
 catch (Exception $e)
 {
-	print'ただいま障害により大変ご迷惑をお掛けしております。';
-	exit();
+    print'ただいま障害により大変ご迷惑をお掛けしております。';
+    exit();
 }
+
 ?>
 
 </body>
