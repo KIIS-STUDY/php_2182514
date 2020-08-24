35fdf7144ff1d20923d9e112af67c066b033c96a
Ryoukenhou <liujianfeng6y@gmail.com>
2020-08-24 14:10:11 +0900

    2020.08.24

---
+++ ehon/echo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE heml>
+<html lang="ja">
+    <head>
+        <meta charset="UTE-8">
+        <title>Hello</title>
+    </head>
+<body>
+    <p>
+        <?php
+    echo "Hello World!"
+    ?>
+    </p>
+    </body>
+</html>
---
+++ ehon/hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE heml>
+<html lang="ja">
+    <head>
+        <meta charset="UTE-8">
+        <title>Greeting</title>
+    </head>
+<body>
+    Hello HTML!
+    </body>
+</html>
